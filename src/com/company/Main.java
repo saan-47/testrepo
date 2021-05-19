11f54ebe31d679b7213204add11a332fe51372e8
Hassan <syed.hassan.ibrahim53@gmail.com>
2021-05-18 18:18:37 -0700

    sec commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.printf("This is checking");
+        System.out.printf("Update");
     }
 }
